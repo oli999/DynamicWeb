d43c242efa97c764597e1f7b00e6ddb7089f7c4b
sungwon <zhtmr123@gmail.com>
2019-06-04 11:51:37 +0900

    ddd aaa

---
+++ DynamicWeb/src/test/pac/DDD.java
@@ -0,0 +1,7 @@
+package test.pac;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface DDD {
+	public void AAA();
+}
---
+++ DynamicWeb/src/test/pac/DDDImpl.java
@@ -0,0 +1,13 @@
+package test.pac;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DDDImpl implements DDD{
+
+	@Override
+	public void AAA() {
+		System.out.println("AAA");
+		
+	}
+
+}
